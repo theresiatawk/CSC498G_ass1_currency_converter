2a68a6683a664da17a71c48560ea578078902564
Theresia Tawk <theresiatawk@hotmail.com>
2022-01-30 12:57:22 +0200

    Implemented the UI

---
+++ app/src/main/java/com/lau/ass1currencyconverter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lau.ass1currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
