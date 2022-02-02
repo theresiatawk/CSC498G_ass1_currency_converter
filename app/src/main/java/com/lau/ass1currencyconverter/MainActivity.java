cc2b68cf840d5d731c992ca58d0b4d1b04750beb
Theresia Tawk <theresiatawk@hotmail.com>
2022-02-02 10:14:30 +0200

    Added images to the UI

--- app/src/main/java/com/lau/ass1currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/ass1currencyconverter/MainActivity.java
@@ -3,12 +3,26 @@ package com.lau.ass1currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usd_input;
+    EditText lbp_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usd_input = (EditText) findViewById(R.id.usd_id);
+        lbp_input = (EditText) findViewById(R.id.lbp_id);
+    }
+    public void onConvert(View v){
+//        int usd_rate = 22000;
+//        String usd_value = usd_input.getText().toString();
+//        int i = Integer.valueOf((String) usd_value);
+//        int value_converted_to_lbp = i * usd_rate;
+//        lbp_input.setText(value_converted_to_lbp);
+        Toast.makeText(getApplicationContext(), "Hello",Toast.LENGTH_LONG).show();
     }
 }
